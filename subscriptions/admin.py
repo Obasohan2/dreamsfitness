d0ca26a9649a9a9fe081d6070f0a8d0218bd80c6
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-10-18 00:59:21 +0100

    Added subscription model and admin.py

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.SubPlan)
+class SubPlanAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'max_member', 'validity_days', 'highlight_status')
+    list_editable = ('highlight_status', 'max_member')
+
+
+@admin.register(models.SubPlanFeature)
+class SubPlanFeatureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subplans')
+
+    def subplans(self, obj):
+        return " | ".join(sub.title for sub in obj.subplan.all())
+    subplans.short_description = 'Associated Plans'
--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Subscription Plans
+class SubPlan(models.Model):
+	title = models.CharField(max_length=150)
+	price = models.IntegerField()
+	max_member = models.IntegerField(null=True)
+	highlight_status = models.BooleanField(default=False,null=True)
+	validity_days = models.IntegerField(null=True)
+
+	def __str__(self):
+		return self.title
+
+
+# Subscription Plans Features
+class SubPlanFeature(models.Model):
+	subplan = models.ManyToManyField(SubPlan)
+	title = models.CharField(max_length=150)
+
+	def __str__(self):
+		return self.title
